971c2a1d0f4dd8194cfaf518ad16b59190ae5392
Ferhat yigit Aydin <faydin@k1m05s01.42istanbul.com.tr>
2022-11-22 07:48:55 +0300

    module02

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(void)
+{
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringREF = &s;
+    std::string &stringPTR = s;
+    std::cout << "s: " << &s << std::endl;
+    std::cout << "stringREF: " << &stringREF << std::endl;
+    std::cout << "stringPTR: " << &stringPTR << std::endl;
+    std::cout << std::endl;
+    std::cout << "s: " << s << std::endl;
+    std::cout << "stringREF: " << *stringREF << std::endl;
+    std::cout << "stringPTR: " << stringPTR << std::endl;
+    return 0;
+}
