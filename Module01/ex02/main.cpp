0e97259d419b5e8878b726c120adf6cb97aa5175
Ferhat yigit Aydin <faydin@k1m05s01.42istanbul.com.tr>
2022-11-22 08:46:50 +0300

    module01

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -6,8 +6,7 @@ int main(void)
     std::string &stringPTR = s;
     std::cout << "s: " << &s << std::endl;
     std::cout << "stringREF: " << &stringREF << std::endl;
-    std::cout << "stringPTR: " << &stringPTR << std::endl;
-    std::cout << std::endl;
+    std::cout << "stringPTR: " << &stringPTR << std::endl << std::endl;
     std::cout << "s: " << s << std::endl;
     std::cout << "stringREF: " << *stringREF << std::endl;
     std::cout << "stringPTR: " << stringPTR << std::endl;
