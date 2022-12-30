2b7b1159835b84c696586b16a808b00b236d9a50
Ferhat yigit Aydin <faydin@k1m05s01.42istanbul.com.tr>
2022-12-30 15:37:15 +0300

    %100

---
+++ Module05/ex01/Form.cpp
@@ -0,0 +1,25 @@
+#include "Form.hpp"
+
+Form::Form():name("Saksay Abe"),execute(150),required(150),isSigned(false)
+{
+    std::cout << "Called Constructer" << std::endl;
+}
+
+Form::~Form()
+{
+    std::cout << "Removed Constructer" << std::endl;
+}
+
+Form::Form(std::string name, int execute, int required):name(name),execute(execute),required(required),isSigned(false)
+{
+    std::cout << "Name Called Constructer" << std::endl;
+}
+
+void Form::beSigned(const Bureaucrat &var) const {
+    if (var.getGrade() > this->required)
+        throw GradeTooLowException();
+    else {
+        this->isSigned = true;
+        std::cout << "Bureaucrat signed the form" << std::endl;
+    }
+}
